dfc3f522c2e7cf6a3e308580eb192ea1afa8dc30
Daniel Oliveira <danielbarrosdeoliveira@outlook.com>
2025-04-06 00:46:43 -0300

    🔧 adjustment config container on tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+      },
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
         davano: {
-          primary: "#19FFE7",
-          "primary-dark": "#4AAAA0",
-          secondary: "#FE6219",
-          "secondary-light": "#FF8E59",
-          brown: "#BF6A43",
-          "brown-dark": "#805D4D",
-          "primary-darken": "#415553"
-        }
+          primary: '#19FFE7',
+          'primary-dark': '#4AAAA0',
+          secondary: '#FE6219',
+          'secondary-light': '#FF8E59',
+          brown: '#BF6A43',
+          'brown-dark': '#805D4D',
+          'primary-darken': '#415553',
+        },
       },
       fontFamily: {
         playfair: ['var(--font-playfair)', 'serif'],
@@ -28,4 +34,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
